3349ee4618f2e1c5b1dc4b20df8b24e3cf93bfc5
andreww1219 <138897021+AndrewWang1219@users.noreply.github.com>
2024-12-01 16:49:39 +0800

    feat: parquet2csv脚本

--- src/main/java/cn/edu/szu/aicourse/common/python/PythonEnvActivationAspect.java
+++ src/main/java/cn/edu/szu/aicourse/common/python/PythonEnvActivationAspect.java
@@ -15,5 +15,6 @@ public class PythonEnvActivationAspect {
         // 在这里激活Python环境
         log.info("Activating Python environment before method execution.");
         // 例如，设置环境变量，启动Python进程等
+
     }
 }
--- src/main/resources/scripts/parquet2csv.py
+++ src/main/resources/scripts/parquet2csv.py
@@ -1,10 +1,9 @@
 import os
 import pandas as pd
 import csv
-
+import argparse
 # Define the directory containing Parquet files
-parquet_dir = './output/20240820-004338/artifacts'
-csv_dir = './output/20240820-004338/csv'
+
 
 
 # Function to clean and properly format the string fields
@@ -17,22 +16,29 @@ def clean_quotes(value):
             value = f'"{value}"'
     return value
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_dir', type=str, help='输出文件夹')
+    args = parser.parse_args()
+    output_dir = args.output_dir
 
-# Convert all Parquet files to CSV
-for file_name in os.listdir(parquet_dir):
-    if file_name.endswith('.parquet'):
-        parquet_file = os.path.join(parquet_dir, file_name)
-        csv_file = os.path.join(csv_dir, file_name.replace('.parquet', '.csv'))
+    parquet_dir = os.path.join(output_dir, 'artifacts')
+    csv_dir = os.path.join(output_dir, 'csv')
+    # Convert all Parquet files to CSV
+    for file_name in os.listdir(parquet_dir):
+        if file_name.endswith('.parquet'):
+            parquet_file = os.path.join(parquet_dir, file_name)
+            csv_file = os.path.join(csv_dir, file_name.replace('.parquet', '.csv'))
 
-        # Load the Parquet file
-        df = pd.read_parquet(parquet_file)
+            # Load the Parquet file
+            df = pd.read_parquet(parquet_file)
 
-        # Clean quotes in string fields
-        for column in df.select_dtypes(include=['object']).columns:
-            df[column] = df[column].apply(clean_quotes)
+            # Clean quotes in string fields
+            for column in df.select_dtypes(include=['object']).columns:
+                df[column] = df[column].apply(clean_quotes)
 
-        # Save to CSV
-        df.to_csv(csv_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
-        print(f"Converted {parquet_file} to {csv_file} successfully.")
+            # Save to CSV
+            df.to_csv(csv_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
+            print(f"Converted {parquet_file} to {csv_file} successfully.")
 
-print("All Parquet files have been converted to CSV.")
+    print("All Parquet files have been converted to CSV.")
